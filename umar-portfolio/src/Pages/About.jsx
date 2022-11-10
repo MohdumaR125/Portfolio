b80143da153259d65bb3adad4f1aaebd680d3353
MohdumaR125 <umar231294@gmail.com>
2022-11-10 11:33:10 +0530

    skills part done

--- umar-portfolio/src/Pages/About.jsx
+++ umar-portfolio/src/Pages/About.jsx
@@ -1,9 +1,20 @@
-const About = ()=>{
+import Styles from "../Components/Styles.module.css"
+import coding from "../download/coding.gif"
 
+
+const About = ()=>{
 
     
     return(
-        <div>About</div>
+        <div className={Styles.about}>
+            <div><img src={coding} alt="" /></div>
+            <div>
+                <ul>Hello, My name is Mohd Umar and I enjoy creating thing through my code.</ul>
+                <ul><b>Full Stack Developer</b> with hands on experience in building React Apps. Works efficiently both in frontend as well as backend with good problem solving skills.</ul>
+                <ul>Quick learner and an aspiring full-stack web developer with core knowledge of MERN stack technology.</ul>
+                <ul>I build three major projects. Learned alot about Team work, Time Management and collaboration.</ul>
+            </div>
+        </div>
     )
 }
 export default About
