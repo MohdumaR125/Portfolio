e6232d6e3360a51cb58159cfbacd8236464d7495
MohdumaR125 <umar231294@gmail.com>
2022-11-11 22:24:31 +0530

    contacts updated

--- umar-portfolio/src/Pages/Contact.jsx
+++ umar-portfolio/src/Pages/Contact.jsx
@@ -1,6 +1,23 @@
 const Contact = () =>{
  return(
-    <div>Contact</div>
+    <div>
+      <h1>Get In Touch</h1>
+      <div>IMAGE</div>
+      <div>
+         <div>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+         </div>
+         <form action="Submit">
+            <input type="text" name="" id="" />
+            <input type="text" name="" id="" />
+            <input type="text" name="" id="" />
+            <input type="text" name="" id="" />
+         </form>
+      </div>
+    </div>
  )
 }
 export default Contact
